3287a8215dac13ce5f9b204f0e6986817d534e7c
irfan-web-dev <irfan.dev03@gmail.com>
2025-08-08 13:21:17 +0500

    fixed cors issue

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://portfolio-frontend-sepia-nu.vercel.app/",
+      "https://portfolio-frontend-sepia-nu.vercel.app",
     ], // Your frontend URL
     methods: ["GET", "POST"],
     credentials: true,
